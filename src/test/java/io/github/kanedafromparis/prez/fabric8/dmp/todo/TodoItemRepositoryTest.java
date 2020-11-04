73aa2c2e8d034fae3c7dfd642714e156e4ca08cb
Charles Sabourdin <kanedafromparis@gmail.com>
2020-11-04 13:21:23 +0100

    correct test with new version

--- src/test/java/io/github/kanedafromparis/prez/fabric8/dmp/todo/TodoItemRepositoryTest.java
+++ src/test/java/io/github/kanedafromparis/prez/fabric8/dmp/todo/TodoItemRepositoryTest.java
@@ -15,11 +15,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 @DataJpaTest
 public class TodoItemRepositoryTest {
 
